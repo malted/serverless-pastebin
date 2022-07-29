34cc5d7071bc594c8532103ac1d33c5217f884a2
Malted <malted@malted.dev>
2022-07-29 14:30:53 +0100

    ✨ Add read capabilities

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 export default function handler(request, response) {
 	const { key, value } = request.query;
-	process.env[key] = value;
+	if (value) process.env[key] = value;
 	response.status(200).send(`${key}:${process.env[key]}`);
 }
